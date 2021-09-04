b9f6d73fdca63771e7f75bc88d85dc656fcc1cee
anaaki <naoakitaira@gmail.com>
2021-09-04 22:17:06 +0900

    字句解析対象を決める関数やILLEGALトークンの排出を記述

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,7 +1,6 @@
 package lexer
 
 import (
-	"fmt"
 	"monkey/token"
 )
 
@@ -51,6 +50,14 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+
+	default:
+		if isLitter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
@@ -59,4 +66,19 @@ func (l *Lexer) NextToken() token.Token {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifier() string {
+	// let a = のlからtまでを読み進める。 tの隣は空白,つまりisLetter==Falseにすることで
+	// 字句の切れ目を検出している
+	position := l.position
+	for isLitter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+func isLitter(ch byte) bool {
+	// この企画では英数字だけを対象にする。アンダースコアは例外的にこの言語で許可した範囲とする
+	// foo_barみたいな変数が作れるようになる。
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
