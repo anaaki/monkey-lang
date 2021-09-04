9f68696898e4eed04cb7b4849693e41a2204c019
anaaki <naoakitaira@gmail.com>
2021-09-04 10:00:40 +0900

    LexerのNewを定義。とりあえず字句を読み進めるreadCharだけ追加

--- token/token.go
+++ token/token.go
@@ -32,3 +32,27 @@ const (
 	FUNCTION = "fn"
 	LET      = "let"
 )
+
+type Lexer struct {
+	input        string
+	position     int // 入力における現在位置(現在の文字)
+	readPosition int // これから読み込む位置（現在の文字の次）
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		// 最後まで読んだか終端チェック。終端は0とする 0はASCIIのNULLに対応しているので0を使う。
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
