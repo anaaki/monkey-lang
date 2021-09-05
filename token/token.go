815119a18295d92434efbca893d0256af80df4c5
anaaki <naoakitaira@gmail.com>
2021-09-05 23:28:54 +0900

    true false if else returnの解析を実装

--- token/token.go
+++ token/token.go
@@ -35,13 +35,23 @@ const (
 	RBRACE = "}"
 
 	// キーワード
-	FUNCTION = "fn"
-	LET      = "let"
+	FUNCTION = "FN"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
