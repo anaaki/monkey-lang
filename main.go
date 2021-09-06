d404f3323f709a5536236394bd276167e728e088
anaaki <naoakitaira@gmail.com>
2021-09-07 00:14:45 +0900

    replを追加

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"monkey/repl"
+	"os"
 	"os/user"
 )
 
 func main() {
-	x, _ := user.Current()
+	x, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
 	fmt.Printf("Hello %s This is Monkey programming language \n", x.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
