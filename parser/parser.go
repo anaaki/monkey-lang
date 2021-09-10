fbd748548a449b63ebfc332a44203d46c8bef305
anaaki <naoakitaira@gmail.com>
2021-09-11 02:48:10 +0900

    let のパーサーを追加

--- parser/parser.go
+++ parser/parser.go
@@ -28,5 +28,62 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	// ここにいる時点でcutToken.Typeはtoken.LET
+	stmt := &ast.LetStatement{Token: p.curToken}
+	// let x となるはず。構文をここで強制するような仕組みになっている。
+	// ｘ(IDENT)であればexpectPeek内部でnextToken()されるので、次に読み進めている
+	// expectPeekはアサーション関数と呼ばれる。次が期待する正しい型だと確認できれば読み進める　という動作をする。
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+	// let x = となるはず。
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+	// letは先頭に出てくる。testLetStatementをGreenにするため、letが確認できたらセミコロンまで読み飛ばしてしまっている。
+	// TODO: あとでなおす
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
 }
