eb37198dd71954a02deba33a7f7eee6c658315bd
anaaki <naoakitaira@gmail.com>
2021-10-10 23:57:59 +0900

    -は整数のみにつけられる前置演算子

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -50,6 +50,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusOperatorExpression(right)
 	default:
 		return NULL
 	}
@@ -65,4 +67,12 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	default:
 		return FALSE
 	}
+}
+
+func evalMinusOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value} // -は整数のみ対応とするのでこの書き方になる。
 }
