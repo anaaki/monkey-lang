3bd4842197686105ef743cc789309ebb692e6429
anaaki <naoakitaira@gmail.com>
2021-10-10 23:47:38 +0900

    3.5.4　前置式を実装。 !は右を再起的に読む

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -19,6 +19,9 @@ func Eval(node ast.Node) object.Object {
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	// 式
 	case *ast.Boolean:
 		return nativeBooltoBooleanObject(node.Value)
@@ -41,4 +44,25 @@ func nativeBooltoBooleanObject(input bool) *object.Boolean {
 		return TRUE
 	}
 	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
