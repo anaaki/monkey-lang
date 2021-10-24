91f13f8d7b87b4e37539b30a37aef63b238ea819
anaaki <naoakitaira@gmail.com>
2021-10-24 22:21:58 +0900

    変数束縛と評価を実装

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -51,15 +51,24 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		if isEerror(val) {
 			return val
 		}
+		env.Set(node.Name.Value, val)
 		// 式
 	case *ast.Boolean:
 		return nativeBooltoBooleanObject(node.Value)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Identifier:
+		return evalIdentifier(node, env)
 	}
 	return nil
 }
-
+func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
+	val, ok := env.Get(node.Value)
+	if !ok {
+		return newError("identifier not found: " + node.Value)
+	}
+	return val
+}
 func evalIfExpression(ie *ast.IfExpression, env *object.Environment) object.Object {
 	condition := Eval(ie.Condition, env)
 	if isEerror(condition) {
