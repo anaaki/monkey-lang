8ee398889b48636c1f2b5d82176376e1be058b4b
anaaki <naoakitaira@gmail.com>
2021-11-14 01:15:09 +0900

    4.5.4 ハッシュの評価、格納を実装

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -92,8 +92,31 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return elements[0]
 		}
 		return &object.Array{Elements: elements}
+	case *ast.HashLiteral:
+		return evalHashLiteral(node, env)
 	}
 	return nil
+}
+func evalHashLiteral(node *ast.HashLiteral, env *object.Environment) object.Object {
+	pairs := make(map[object.HashKey]object.HashPair)
+	for keyNode, valueNode := range node.Pairs {
+		key := Eval(keyNode, env)
+		if isEerror(key) {
+			return key
+		}
+		hashKey, ok := key.(object.Hashable)
+		if !ok {
+			return newError("unsusable as hash key: %s", key.Type())
+		}
+
+		value := Eval(valueNode, env)
+		if isEerror(value) {
+			return value
+		}
+		hashed := hashKey.HashKey()
+		pairs[hashed] = object.HashPair{Key: key, Value: value}
+	}
+	return &object.Hash{Paris: pairs}
 }
 func applyFunction(fn object.Object, args []object.Object) object.Object {
 	switch fn := fn.(type) {
